f94c3ae18396e6313922c36291a0766b9f730dda
000hcl <mnrjohansson@gmail.com>
2026-04-09 16:46:39 +0300

    ex 10.9

--- part1/rate-repository-app/src/components/SignIn.jsx
+++ part1/rate-repository-app/src/components/SignIn.jsx
@@ -1,3 +1,4 @@
+import * as yup from 'yup';
 import Text from './Text';
 import { TextInput, Pressable, View, StyleSheet } from 'react-native';
 import { useFormik } from 'formik';
@@ -14,46 +15,87 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     padding: 6,
     paddingLeft: 10,
-    marginBottom:10
+    margin: 5
+
   },
   button: {
     backgroundColor: theme.colors.primary,
     borderRadius: 3,
     padding: 10,
-    alignItems: 'center'
+    alignItems: 'center',
+    margin: 5
+  },
+  errorInput: {
+    borderWidth: 2,
+    borderColor: theme.colors.errorRed,
+    borderRadius: 3,
+    padding: 6,
+    paddingLeft: 10,
+    margin: 5
+
   }
 })
+
+const getInputStyle = (touched, error) => {
+  if (touched && error) {
+    return styles.errorInput
+  } else {
+    return styles.input
+  }
+}
 
 const initialValues = {
   username: '',
   password: ''
 }
 
+const validationSchema = yup.object().shape({
+  username: yup
+    .string().required(),
+  password: yup
+    .string().required()
+})
+
+//  {formik.touched.mass && formik.errors.mass && (
+//   <Text style={{ color: 'red' }}>{formik.errors.mass}</Text>
+//  )}
+
 const SignInForm = ({ onSubmit }) => {
   const formik = useFormik({
     initialValues,
+    validationSchema,
     onSubmit
   })
   return (
     <View style={styles.bigBox}>
       <TextInput
-        style={styles.input} 
+        style={getInputStyle(formik.touched.username, formik.errors.username)} 
         placeholder='username'
         value={formik.values.username}
+        onBlur={formik.handleBlur('username')}
         onChangeText={formik.handleChange('username')}
       />
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{color:theme.colors.errorRed}}>{formik.errors.username}</Text>
+      )}
       <TextInput
       secureTextEntry
-      style={styles.input} 
+      style={getInputStyle(formik.touched.password, formik.errors.password)} 
       placeholder='password'
       value={formik.values.password}
+      onBlur={formik.handleBlur('password')}
       onChangeText={formik.handleChange('password')}
       />
-      <View style={styles.button}>
-        <Pressable onPress={formik.handleSubmit}>
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{color:theme.colors.errorRed}}>{formik.errors.password}</Text>
+      )}
+      <Pressable onPress={formik.handleSubmit}>
+        <View style={styles.button}>
           <Text fontWeight={'bold'} color={'white'}>Sign in</Text>
-        </Pressable>
-      </View>
+        </View>
+
+      </Pressable>
+
 
     </View>
 
